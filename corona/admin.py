83fd8c183908bac064c65721cbee2681c0a0b0f7
coderjaymoh <lelerukjaymoh@gmail.com>
2020-03-26 18:22:27 +0300

    Fetched sms balance from africas talking and displayed in dashboard

--- corona/admin.py
+++ corona/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipient, Language, Donation, Volunteer, Message, VernacularMessage, SMSDonation
+from .models import Recipient, Language, Donation, Volunteer, Message, VernacularMessage, SMSDonation, SMSBalance
 
 admin.site.register(Recipient)
 admin.site.register(Language)
@@ -8,3 +8,4 @@ admin.site.register(Volunteer)
 admin.site.register(Message)
 admin.site.register(VernacularMessage)
 admin.site.register(SMSDonation)
+admin.site.register(SMSBalance)
